5a214076c2e0eb46b83a05bc0c7b8c6af6f6fdd2
Tibor Gerlai <tibor.gerlai@arm.com>
2026-08-24 15:26:05 +0200

    add global config for stylua

--- wezterm/config.lua
+++ wezterm/config.lua
@@ -10,7 +10,7 @@ local user = os.getenv("USER")
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 config.automatically_reload_config = true
@@ -27,190 +27,190 @@ config.font_size = 18
 -- Find the nicest font possible
 --config.font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Bold" })
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrains Mono", weight = "Bold" },
-	"Noto Sans Symbols 2",
-	"Noto Sans Math",
-	"Symbols Nerd Font Mono",
+    { family = "JetBrains Mono", weight = "Bold" },
+    "Noto Sans Symbols 2",
+    "Noto Sans Math",
+    "Symbols Nerd Font Mono",
 })
 
 config.enable_tab_bar = true
 
 config.leader = {
-	key = "g",
-	mods = "CTRL",
-	timeout_milliseconds = 2000,
+    key = "g",
+    mods = "CTRL",
+    timeout_milliseconds = 2000,
 }
 
 config.keys = {
-	{
-		key = ";",
-		mods = "LEADER",
-		action = act.ActivateCopyMode,
-	},
-	{
-		key = "q",
-		mods = "SUPER",
-		action = act.DisableDefaultAssignment,
-	},
-	{
-		key = "z",
-		mods = "LEADER",
-		action = act.TogglePaneZoomState,
-	},
-	{
-		key = "c",
-		mods = "LEADER",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "n",
-		mods = "LEADER",
-		action = act.ActivateTabRelative(1),
-	},
-	{
-		key = "p",
-		mods = "LEADER",
-		action = act.ActivateTabRelative(-1),
-	},
-	{
-		key = ",",
-		mods = "LEADER",
-		action = act.PromptInputLine({
-			description = "Enter new name for tab",
-			action = wezterm.action_callback(function(window, pane, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-	{
-		key = "w",
-		mods = "LEADER",
-		action = act.ShowTabNavigator,
-	},
-	-- Close tab
-	{
-		key = "&",
-		mods = "LEADER|SHIFT",
-		action = act.CloseCurrentTab({ confirm = true }),
-	},
-	-- Hyper + (h,j,k,l) to move between panes
-	{
-		key = "h",
-		mods = "ALT|SHIFT|CTRL|SUPER",
-		action = act({ EmitEvent = "move-left" }),
-	},
-	{
-		key = "j",
-		mods = "ALT|SHIFT|CTRL|SUPER",
-		action = act({ EmitEvent = "move-down" }),
-	},
-	{
-		key = "k",
-		mods = "ALT|SHIFT|CTRL|SUPER",
-		action = act({ EmitEvent = "move-up" }),
-	},
-	{
-		key = "l",
-		mods = "ALT|SHIFT|CTRL|SUPER",
-		action = act({ EmitEvent = "move-right" }),
-	},
-	-- ALT + (h,j,k,l) to resize panes
-	{
-		key = "h",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-left" }),
-	},
-	{
-		key = "j",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-down" }),
-	},
-	{
-		key = "k",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-up" }),
-	},
-	{
-		key = "l",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-right" }),
-	},
-	-- Vertical split
-	{
-		-- |
-		key = "\\",
-		mods = "LEADER",
-		action = act.SplitPane({
-			direction = "Right",
-			size = { Percent = 50 },
-		}),
-	},
-	-- Horizontal split
-	{
-		-- -
-		key = "-",
-		mods = "LEADER",
-		action = act.SplitPane({
-			direction = "Down",
-			size = { Percent = 50 },
-		}),
-	},
-	{
-		-- |
-		key = "{",
-		mods = "LEADER|SHIFT",
-		action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
-	},
-	-- Attach to muxer
-	{
-		key = "a",
-		mods = "LEADER",
-		action = act.AttachDomain("unix"),
-	},
+    {
+        key = ";",
+        mods = "LEADER",
+        action = act.ActivateCopyMode,
+    },
+    {
+        key = "q",
+        mods = "SUPER",
+        action = act.DisableDefaultAssignment,
+    },
+    {
+        key = "z",
+        mods = "LEADER",
+        action = act.TogglePaneZoomState,
+    },
+    {
+        key = "c",
+        mods = "LEADER",
+        action = act.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+        key = "n",
+        mods = "LEADER",
+        action = act.ActivateTabRelative(1),
+    },
+    {
+        key = "p",
+        mods = "LEADER",
+        action = act.ActivateTabRelative(-1),
+    },
+    {
+        key = ",",
+        mods = "LEADER",
+        action = act.PromptInputLine({
+            description = "Enter new name for tab",
+            action = wezterm.action_callback(function(window, pane, line)
+                if line then
+                    window:active_tab():set_title(line)
+                end
+            end),
+        }),
+    },
+    {
+        key = "w",
+        mods = "LEADER",
+        action = act.ShowTabNavigator,
+    },
+    -- Close tab
+    {
+        key = "&",
+        mods = "LEADER|SHIFT",
+        action = act.CloseCurrentTab({ confirm = true }),
+    },
+    -- Hyper + (h,j,k,l) to move between panes
+    {
+        key = "h",
+        mods = "ALT|SHIFT|CTRL|SUPER",
+        action = act({ EmitEvent = "move-left" }),
+    },
+    {
+        key = "j",
+        mods = "ALT|SHIFT|CTRL|SUPER",
+        action = act({ EmitEvent = "move-down" }),
+    },
+    {
+        key = "k",
+        mods = "ALT|SHIFT|CTRL|SUPER",
+        action = act({ EmitEvent = "move-up" }),
+    },
+    {
+        key = "l",
+        mods = "ALT|SHIFT|CTRL|SUPER",
+        action = act({ EmitEvent = "move-right" }),
+    },
+    -- ALT + (h,j,k,l) to resize panes
+    {
+        key = "h",
+        mods = "ALT",
+        action = act({ EmitEvent = "resize-left" }),
+    },
+    {
+        key = "j",
+        mods = "ALT",
+        action = act({ EmitEvent = "resize-down" }),
+    },
+    {
+        key = "k",
+        mods = "ALT",
+        action = act({ EmitEvent = "resize-up" }),
+    },
+    {
+        key = "l",
+        mods = "ALT",
+        action = act({ EmitEvent = "resize-right" }),
+    },
+    -- Vertical split
+    {
+        -- |
+        key = "\\",
+        mods = "LEADER",
+        action = act.SplitPane({
+            direction = "Right",
+            size = { Percent = 50 },
+        }),
+    },
+    -- Horizontal split
+    {
+        -- -
+        key = "-",
+        mods = "LEADER",
+        action = act.SplitPane({
+            direction = "Down",
+            size = { Percent = 50 },
+        }),
+    },
+    {
+        -- |
+        key = "{",
+        mods = "LEADER|SHIFT",
+        action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
+    },
+    -- Attach to muxer
+    {
+        key = "a",
+        mods = "LEADER",
+        action = act.AttachDomain("unix"),
+    },
 
-	-- Detach from muxer
-	{
-		key = "d",
-		mods = "LEADER",
-		action = act.DetachDomain({ DomainName = "unix" }),
-	},
-	-- Rename current session; analagous to command in tmux
-	{
-		key = "$",
-		mods = "LEADER|SHIFT",
-		action = act.PromptInputLine({
-			description = "Enter new name for session",
-			action = wezterm.action_callback(function(window, pane, line)
-				if line then
-					mux.rename_workspace(window:mux_window():get_workspace(), line)
-				end
-			end),
-		}),
-	},
-	-- Show list of workspaces
-	{
-		key = "s",
-		mods = "LEADER",
-		action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
-	},
-	-- Session manager bindings
-	{
-		key = "S",
-		mods = "LEADER|SHIFT",
-		action = act({ EmitEvent = "save_session" }),
-	},
-	{
-		key = "L",
-		mods = "LEADER|SHIFT",
-		action = act({ EmitEvent = "load_session" }),
-	},
-	{
-		key = "R",
-		mods = "LEADER|SHIFT",
-		action = act({ EmitEvent = "restore_session" }),
-	},
+    -- Detach from muxer
+    {
+        key = "d",
+        mods = "LEADER",
+        action = act.DetachDomain({ DomainName = "unix" }),
+    },
+    -- Rename current session; analagous to command in tmux
+    {
+        key = "$",
+        mods = "LEADER|SHIFT",
+        action = act.PromptInputLine({
+            description = "Enter new name for session",
+            action = wezterm.action_callback(function(window, pane, line)
+                if line then
+                    mux.rename_workspace(window:mux_window():get_workspace(), line)
+                end
+            end),
+        }),
+    },
+    -- Show list of workspaces
+    {
+        key = "s",
+        mods = "LEADER",
+        action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
+    },
+    -- Session manager bindings
+    {
+        key = "S",
+        mods = "LEADER|SHIFT",
+        action = act({ EmitEvent = "save_session" }),
+    },
+    {
+        key = "L",
+        mods = "LEADER|SHIFT",
+        action = act({ EmitEvent = "load_session" }),
+    },
+    {
+        key = "R",
+        mods = "LEADER|SHIFT",
+        action = act({ EmitEvent = "restore_session" }),
+    },
 }
 
 -- Put the tab bar at the bottom
@@ -233,93 +233,93 @@ local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 -- or `wezterm cli set-tab-title`, but falls back to the
 -- title of the active pane in that tab.
 function tab_title(tab_info)
-	local title = tab_info.tab_title
-	-- if the tab title is explicitly set, take that
-	if title and #title > 0 then
-		return tab_info.tab_index + 1 .. ":" .. title
-	end
-	-- Otherwise, use the title from the active pane
-	-- in that tab
-	return tab_info.active_pane.title
+    local title = tab_info.tab_title
+    -- if the tab title is explicitly set, take that
+    if title and #title > 0 then
+        return tab_info.tab_index + 1 .. ":" .. title
+    end
+    -- Otherwise, use the title from the active pane
+    -- in that tab
+    return tab_info.active_pane.title
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local edge_background = "#313244"
-	local background = "#313244"
-	local foreground = "#cdd6f4"
+    local edge_background = "#313244"
+    local background = "#313244"
+    local foreground = "#cdd6f4"
 
-	if tab.is_active then
-		-- I use a solarized dark theme; this gives a teal background to the active tab
-		foreground = "#073642"
-		background = "#2aa198"
-	elseif hover then
-		background = "#45475a"
-		foreground = "#f5e0dc"
-	end
+    if tab.is_active then
+        -- I use a solarized dark theme; this gives a teal background to the active tab
+        foreground = "#073642"
+        background = "#2aa198"
+    elseif hover then
+        background = "#45475a"
+        foreground = "#f5e0dc"
+    end
 
-	local edge_foreground = background
+    local edge_foreground = background
 
-	local ICONS = {
-		MAC = "󰀵 ",
-		UBUNTU = "󰕈 ",
-		VM = "󰌽 ",
-		EUHPC = "󱄛 ",
-		EUHPC2 = " ",
-		ProxMox = " ",
-	}
+    local ICONS = {
+        MAC = "󰀵 ",
+        UBUNTU = "󰕈 ",
+        VM = "󰌽 ",
+        EUHPC = "󱄛 ",
+        EUHPC2 = " ",
+        ProxMox = " ",
+    }
 
-	-- Provide default icon for missing keys
-	local DEFAULT_ICON = "󰌽 "
-	setmetatable(ICONS, {
-		__index = function(_, _)
-			return DEFAULT_ICON
-		end,
-	})
+    -- Provide default icon for missing keys
+    local DEFAULT_ICON = "󰌽 "
+    setmetatable(ICONS, {
+        __index = function(_, _)
+            return DEFAULT_ICON
+        end,
+    })
 
-	local ttitle = tab_title(tab)
-	-- 1. Split using pattern match
-	local after_colon = ttitle:match("^[^:]*:(.*)$")
+    local ttitle = tab_title(tab)
+    -- 1. Split using pattern match
+    local after_colon = ttitle:match("^[^:]*:(.*)$")
 
-	-- 2. Remove all whitespace
-	local cleaned = after_colon and after_colon:gsub("%s+", "") or ""
-	local title = ICONS[cleaned] .. ttitle .. "<" .. #panes .. ">"
+    -- 2. Remove all whitespace
+    local cleaned = after_colon and after_colon:gsub("%s+", "") or ""
+    local title = ICONS[cleaned] .. ttitle .. "<" .. #panes .. ">"
 
-	-- ensure that the titles fit in the available space,
-	-- and that we have room for the edges.
-	title = wezterm.truncate_right(title, max_width - 2)
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    title = wezterm.truncate_right(title, max_width - 2)
 
-	return {
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_LEFT_ARROW },
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_RIGHT_ARROW },
-	}
+    return {
+        { Background = { Color = edge_background } },
+        { Foreground = { Color = edge_foreground } },
+        { Text = SOLID_LEFT_ARROW },
+        { Background = { Color = background } },
+        { Foreground = { Color = foreground } },
+        { Text = title },
+        { Background = { Color = edge_background } },
+        { Foreground = { Color = edge_foreground } },
+        { Text = SOLID_RIGHT_ARROW },
+    }
 end)
 
 -- config.color_scheme = "Japanesque"
 
 config.colors = {
-	tab_bar = {
-		new_tab = {
-			bg_color = "#313244",
-			fg_color = "#cdd6f4",
-		},
-		new_tab_hover = {
-			bg_color = "#45475a",
-			fg_color = "#f5e0dc",
-		},
-	},
-	-- Colour of *all* pane split lines
-	split = "#ff5f5f",
+    tab_bar = {
+        new_tab = {
+            bg_color = "#313244",
+            fg_color = "#cdd6f4",
+        },
+        new_tab_hover = {
+            bg_color = "#45475a",
+            fg_color = "#f5e0dc",
+        },
+    },
+    -- Colour of *all* pane split lines
+    split = "#ff5f5f",
 }
 config.inactive_pane_hsb = {
-	saturation = 0.9,
-	brightness = 0.4,
+    saturation = 0.9,
+    brightness = 0.4,
 }
 -- Switch to the last active tab when I close a tab
 config.switch_to_last_active_tab_when_closing_tab = true
@@ -327,10 +327,10 @@ config.switch_to_last_active_tab_when_closing_tab = true
 config.scrollback_lines = 10000
 -- I don't really have need for padding between panes
 config.window_padding = {
-	left = 6,
-	right = 6,
-	top = 4,
-	bottom = 4,
+    left = 6,
+    right = 6,
+    top = 4,
+    bottom = 4,
 }
 -- small extra padding inside the terminal cells for a “boxed” feel
 -- (this makes everything look less cramped, which makes the borders feel cleaner)
@@ -338,113 +338,113 @@ config.window_padding = {
 -- config.cell_width = 0.95
 
 local move_around = function(window, pane, direction_wez, direction_nvim)
-	local result = os.execute(
-		"env NVIM_LISTEN_ADDRESSS=/tmp/nvim"
-			.. paane:pane_did()
-			.. " "
-			.. weztterm.home_dir
-			.. "/.local/bin/wezterm.nviim.navigaotor"
-			.. " "
-			.. direction_nvim
-	)
-	if result then
-		window:perform_action(act({ SendString = "\x17" .. direction_nvim }), pane)
-	else
-		window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
-	end
+    local result = os.execute(
+        "env NVIM_LISTEN_ADDRESSS=/tmp/nvim"
+            .. paane:pane_did()
+            .. " "
+            .. weztterm.home_dir
+            .. "/.local/bin/wezterm.nviim.navigaotor"
+            .. " "
+            .. direction_nvim
+    )
+    if result then
+        window:perform_action(act({ SendString = "\x17" .. direction_nvim }), pane)
+    else
+        window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
+    end
 end
 
 wezterm.on("move-left", function(window, pane)
-	move_around(window, pane, "Left", "h")
+    move_around(window, pane, "Left", "h")
 end)
 
 wezterm.on("move-right", function(window, pane)
-	move_around(window, pane, "Right", "l")
+    move_around(window, pane, "Right", "l")
 end)
 
 wezterm.on("move-up", function(window, pane)
-	move_around(window, pane, "Up", "k")
+    move_around(window, pane, "Up", "k")
 end)
 
 wezterm.on("move-down", function(window, pane)
-	move_around(window, pane, "Down", "j")
+    move_around(window, pane, "Down", "j")
 end)
 
 local vim_resize = function(window, pane, direction_wez, direction_nvim)
-	local result = os.execute(
-		"env NVIM_LISTEN_ADDRESSS=/tmp/nvim"
-			.. paane:pane_did()
-			.. " "
-			.. weztterm.home_dir
-			.. "/.local/bin/wezterm.nviim.navigaotor"
-			.. " "
-			.. direction_nvim
-	)
-	if result then
-		window:perform_action(act({ SendString = "\x1b" .. direction_nvim }), pane)
-	else
-		window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
-	end
+    local result = os.execute(
+        "env NVIM_LISTEN_ADDRESSS=/tmp/nvim"
+            .. paane:pane_did()
+            .. " "
+            .. weztterm.home_dir
+            .. "/.local/bin/wezterm.nviim.navigaotor"
+            .. " "
+            .. direction_nvim
+    )
+    if result then
+        window:perform_action(act({ SendString = "\x1b" .. direction_nvim }), pane)
+    else
+        window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
+    end
 end
 
 wezterm.on("resize-left", function(window, pane)
-	vim_resize(window, pane, "Left", "h")
+    vim_resize(window, pane, "Left", "h")
 end)
 
 wezterm.on("resize-right", function(window, pane)
-	vim_resize(window, pane, "Right", "l")
+    vim_resize(window, pane, "Right", "l")
 end)
 
 wezterm.on("resize-up", function(window, pane)
-	vim_resize(window, pane, "Up", "k")
+    vim_resize(window, pane, "Up", "k")
 end)
 
 wezterm.on("resize-down", function(window, pane)
-	vim_resize(window, pane, "Down", "j")
+    vim_resize(window, pane, "Down", "j")
 end)
 
 -- from: https://akos.ma/blog/adopting-wezterm/
 config.hyperlink_rules = {
-	-- Matches: a URL in parens: (URL)
-	{
-		regex = "\\((\\w+://\\S+)\\)",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in brackets: [URL]
-	{
-		regex = "\\[(\\w+://\\S+)\\]",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in curly braces: {URL}
-	{
-		regex = "\\{(\\w+://\\S+)\\}",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in angle brackets: <URL>
-	{
-		regex = "<(\\w+://\\S+)>",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Then handle URLs not wrapped in brackets
-	{
-		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-		format = "$1",
-		highlight = 1,
-	},
+    -- Matches: a URL in parens: (URL)
+    {
+        regex = "\\((\\w+://\\S+)\\)",
+        format = "$1",
+        highlight = 1,
+    },
+    -- Matches: a URL in brackets: [URL]
+    {
+        regex = "\\[(\\w+://\\S+)\\]",
+        format = "$1",
+        highlight = 1,
+    },
+    -- Matches: a URL in curly braces: {URL}
+    {
+        regex = "\\{(\\w+://\\S+)\\}",
+        format = "$1",
+        highlight = 1,
+    },
+    -- Matches: a URL in angle brackets: <URL>
+    {
+        regex = "<(\\w+://\\S+)>",
+        format = "$1",
+        highlight = 1,
+    },
+    -- Then handle URLs not wrapped in brackets
+    {
+        regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+        format = "$1",
+        highlight = 1,
+    },
 }
 config.background = {
-	-- This is the deepest/back-most layer. It will be rendered first
-	{
-		source = {
-			File = "/Users/" .. user .. "/.config/wezterm/kanagawa-wave.png",
-		},
-		repeat_x = "NoRepeat",
-		hsb = dimmer,
-	},
+    -- This is the deepest/back-most layer. It will be rendered first
+    {
+        source = {
+            File = "/Users/" .. user .. "/.config/wezterm/kanagawa-wave.png",
+        },
+        repeat_x = "NoRepeat",
+        hsb = dimmer,
+    },
 }
 
 return config
